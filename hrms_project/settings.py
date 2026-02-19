1339b746f43d042fbbbdad5368db8f91137c7da5
emtainfotech <info@emtainfotech.com>
2026-02-19 13:17:33 +0530

    Setting.py file changes

--- hrms_project/settings.py
+++ hrms_project/settings.py
@@ -77,12 +77,24 @@ CORS_ALLOW_ALL_ORIGINS = True
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'hrms_db',
+        'USER': 'hrms_admin',
+        'PASSWORD': 'admin123',
+        'HOST': 'localhost',   # or 127.0.0.1
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
